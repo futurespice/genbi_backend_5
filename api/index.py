dbdfc349d565f09ca01eba2bffc27032637bc42f
futurespice <azatss442@gmail.com>
2025-12-10 17:20:22 +0600

    Add Mangum ASGI adapter for Vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
-"""
-Vercel serverless function entry point
-"""
+from mangum import Mangum
 from app.main import app
 
-# Vercel ищет переменную "app" или "handler"
-handler = app
+handler = Mangum(app, lifespan="off")
